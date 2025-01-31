4415b614aea93f580c685520022803435a8e5f50
Vlad-2109 <vladswim21@gmail.com>
2025-01-31 22:06:37 +0200

    add central text

---
+++ src/components/Home/Hero/Hero.tsx
@@ -0,0 +1,29 @@
+const Hero = () => {
+	return (
+		<div className="relative w-full h-[120vh] sm:h-[100vh]">
+			{/* Overlay */}
+			<div className="absolute top-0 left-0 w-full h-full bg-gray-800 opacity-70"></div>
+			{/* Video */}
+			<video
+				src="/images/hero1.mp4"
+				autoPlay
+				muted
+				loop
+				preload="metadata"
+				className="w-full h-full object-cover"
+			/>
+			{/* Text content */}
+			<div className="absolute z-[100] w-full h-full top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+				<div className="flex items-center justify-center flex-col w-full h-full">
+					<div>
+						<h1 className="text-[25px] mb-4 md:mb-0 text-center md:text-[35px] lg:text-[45px] tracking-[0.7rem] text-white font-bold uppercase">
+							LET&apos;S ENJOT THE NATURE
+						</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
